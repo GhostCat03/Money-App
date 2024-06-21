286f987c1e2df6f4fc8603e47ac503e13d9d682e
Felicity Matthews <felicitymatthews30@gmail.com>
2024-06-21 17:14:51 +1000

    create transaction page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import DataPage from './pages/data.tsx';
+import Layout from './components/layout.tsx';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Layout/>
+      <DataPage/>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -19,7 +23,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
---
+++ src/pages/data.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+/**
+ * 
+ * @returns the page containing the data logic and display
+ */
+export default function DataPage() {
+    return (
+            <div className="containter px-5 mx-5">
+                <div className="text-start fs-1 pb-2">Transactions</div>
+                <div className="container">
+                    <div className="row">
+                        <div className="col border">
+                            1 of 2
+                        </div>
+                        <div className="col border">
+                            2 of 2
+                        </div>
+                    </div>
+                </div>
+            </div>
+    )
+}
