3e564c6c5c686479eb899872e150cf3f4f2f8bdc
Felicity Matthews <felicitymatthews30@gmail.com>
2024-06-22 20:10:44 +1000

    read in transactions from a csv and convert to a struct

--- pages/pages/data.tsx
+++ pages/pages/data.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import TransactionTable from "../components/transactions/transactionTable";
+import { Transaction } from "../types/Transaction";
 
 // import csv from "./testTransaction.csv"
 // import { parse } from "csv-parse";
@@ -12,25 +13,41 @@ import TransactionTable from "../components/transactions/transactionTable";
  */
 export default function DataPage({ fileName }: { fileName: string }) {
 
-  // const csv = require("./testTransaction.csv")
-  // console.log(csv)
+  // converts a string to a date object
+  const parseDate = (dateString: string): Date => {
+    const splitData = dateString.split('/')
+    return new Date(
+      parseInt(splitData[2]),
+      parseInt(splitData[1]) - 1,
+      parseInt(splitData[0])
+    )
+  }
 
-  const filePath = "./" + fileName;
-  // let data: string[][] = []
+  // creates a transaction struct from a split csv
+  const createTransaction = (data: string[]): Transaction => ({
+    date: parseDate(data[0]),
+    account: data[1],
+    description: data[2],
+    credit: data[3] ? parseFloat(data[3]) : 0,
+    debit: data[4] ? parseFloat(data[4]) : 0,
+  } as Transaction)
 
-  // fetch(filePath).then(response => response.text())
-  // .then(text => {
-  //   var lines = text.split("\r\n");
-  //   lines.forEach((line, index) => {
-  //     if (index > 0) {
-  //       data.push(line.split(','))
-  //     }
-  //   })
-  // })
+  // constructs the file path
+  const filePath = "./data/raw/" + fileName;
+  let data: Transaction[] = []
 
-  // console.log(data)
+  // fetches the data and transforms it to Transaction structs.
+  fetch(filePath).then(response => response.text())
+  .then(text => {
+    var lines = text.split("\r\n");
+    lines.forEach((line, index) => {
+      if (index > 0) {
+        data.push(createTransaction(line.split(',')))
+      }
+    })
+  })
 
-  
+  console.log(data)
 
   return (
     <div className="containter px-5 mx-5">
@@ -38,18 +55,6 @@ export default function DataPage({ fileName }: { fileName: string }) {
         Transactions <span className="fs-4"> - {fileName}</span>
       </div>
       <TransactionTable filePath={filePath} />
-
-      {/* 
-                <div className="container">
-                    <div className="row">
-                        <div className="col border">
-                            1 of 2
-                        </div>
-                        <div className="col border">
-                            2 of 2
-                        </div>
-                    </div>
-                </div> */}
     </div>
   );
 }
---
+++ pages/types/Transaction.tsx
@@ -0,0 +1,8 @@
+// Transaction type 
+export type Transaction = {
+  date: Date;
+  account: string;
+  description: string;
+  credit: number;
+  debit: number;
+}
